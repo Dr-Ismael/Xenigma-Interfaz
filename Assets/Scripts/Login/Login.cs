ab07b9944f8b1ede6ecd36224b2597ceb648f48f
LuisFernandoPP <159959065+LuisFernandoPP@users.noreply.github.com>
2024-04-11 14:55:58 -0700

    Guardar idUsuario  al registro
    Solución de bug, al momento de registrarse ahora también se guarda el id del usuario

--- Assets/Scripts/Login/Login.cs
+++ Assets/Scripts/Login/Login.cs
@@ -198,6 +198,7 @@ public class Login : MonoBehaviour
                 resultadoPassword = MS_Reader.GetString(5);
                 resultadoNickname = MS_Reader.GetString(3);
                 resultadoID = MS_Reader.GetString(0);
+                PlayerPrefs.SetString("idUser", resultadoID);
 
                 Debug.Log(resultadoID + " " + resultadoUser + " " + resultadoPassword);
             }
